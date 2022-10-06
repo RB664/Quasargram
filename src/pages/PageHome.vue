c63a7a9f0595c0146e2a904bd712c6df2c67ce94
RB664 <ridhaabenjamin06@gmail.com>
2022-10-06 15:14:31 +0200

    first commit

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="Ken Kaneki.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Ken Kaneki</q-item-label>
+          <q-item-label caption>
+            Japan
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-separator />
+      <q-img src="Touka.jpg" />
+      <q-card-section>
+        <div class="text-h6">Mine :)</div>
+        <div class="text-subtitle">by Ken Kaneki</div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageHome'
+}
+</script>
+
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+</style>
